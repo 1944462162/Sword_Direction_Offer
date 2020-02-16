b67bccbe79607b41df539b6a5ad176b8b16770b8
SuperCat <1944462162@qq.com>
2020-02-16 19:34:53 +0800

    链表

---
+++ 删除链表中重复的结点/src/Main.java
@@ -0,0 +1,67 @@
+/**
+ * Author: wangJianBo
+ * Date: 2020/2/16 17:47
+ * Content: 链表
+ */
+
+class ListNode {
+    int val;
+    ListNode next = null;
+
+    ListNode(int val) {
+        this.val = val;
+    }
+}
+
+class Solution {
+    public ListNode deleteDuplication(ListNode pHead)
+    {
+        if (pHead == null || pHead.next == null){
+            return pHead;
+        }
+        ListNode preNode = new ListNode(-1);
+        ListNode ans = preNode;
+        preNode.next = pHead;
+        ListNode curNode = pHead;
+
+        while (curNode != null){
+            if (curNode.next != null && curNode.val == curNode.next.val){
+                while (curNode.next != null && curNode.val == curNode.next.val){
+                    curNode = curNode.next;
+                }
+                preNode.next = curNode.next;
+            }
+            else {
+                preNode = curNode;
+            }
+            curNode = curNode.next;
+        }
+        return ans.next;
+    }
+}
+public class Main {
+    public static void main(String[] args) {
+        ListNode listNode = new ListNode(1);
+        ListNode listNode1 = new ListNode(2);
+        ListNode listNode2 = new ListNode(3);
+        ListNode listNode3 = new ListNode(3);
+        ListNode listNode4 = new ListNode(4);
+        ListNode listNode5 = new ListNode(5);
+        ListNode listNode6 = new ListNode(5);
+
+        listNode.next = listNode1;
+        listNode1.next = listNode2;
+        listNode2.next = listNode3;
+        listNode3.next = listNode4;
+        listNode4.next = listNode5;
+        listNode5.next = listNode6;
+
+        Solution solution = new Solution();
+        ListNode listNode7 = solution.deleteDuplication(listNode);
+
+        while (listNode7 != null){
+            System.out.println(listNode7.val);
+            listNode7 = listNode7.next;
+        }
+    }
+}
