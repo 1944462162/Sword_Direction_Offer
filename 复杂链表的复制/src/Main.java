ae48d6f1e2f03eeb1881478d4f3cd4b6d4c113bc
SuperCat <1944462162@qq.com>
2020-02-29 18:04:49 +0800

    链表

--- 复杂链表的复制/src/Main.java
+++ 复杂链表的复制/src/Main.java
@@ -1,7 +1,10 @@
+import java.util.HashMap;
+import java.util.LinkedList;
+
 /**
  * Author: wangJianBo
  * Date: 2020/2/29 14:31
- * Content:
+ * Content: 链表
  */
 
 class RandomListNode {
@@ -16,11 +19,21 @@ class RandomListNode {
 class Solution {
     public RandomListNode Clone(RandomListNode pHead)
     {
-        RandomListNode randomListNode = pHead;
-        RandomListNode randomListNode1 = randomListNode;
-        while (pHead != null){
+        HashMap<RandomListNode,RandomListNode> hashMap = new HashMap<>();
 
+        RandomListNode randomListNode = pHead;
+        while (randomListNode != null){
+            hashMap.put(randomListNode, new RandomListNode(randomListNode.label));
+            randomListNode = randomListNode.next;
         }
+
+        for (RandomListNode listNode : hashMap.keySet()) {
+            RandomListNode randomListNode1 = hashMap.get(listNode);
+            randomListNode1.next = hashMap.get(listNode.next);
+            randomListNode1.random = hashMap.get(listNode.random);
+        }
+
+        return hashMap.get(pHead);
     }
 }
 public class Main {
@@ -40,5 +53,10 @@ public class Main {
         randomListNode3.random = randomListNode4;
 
         RandomListNode clone = solution.Clone(randomListNode1);
+
+        while (clone != null){
+            System.out.println(clone.label);
+            clone = clone.next;
+        }
     }
 }
